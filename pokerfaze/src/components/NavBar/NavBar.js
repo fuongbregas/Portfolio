2396077f2d04b955200a026e293e4e541bed61a2
fuongbregas <fuongbregas@icloud.com>
2021-08-24 22:39:50 -0700

    backup

---
+++ pokerfaze/src/components/NavBar/NavBar.js
@@ -0,0 +1,12 @@
+import './NavBar.css';
+import React from 'react';
+
+const NavBar = () => {
+    return (
+        <nav className = 'navbar'>
+            <div></div>
+        </nav>
+    );
+}
+
+export default NavBar;
