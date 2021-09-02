f63ff5a2bf5b51edfd8fec69b3535d01101c08c4
fuongbregas <fuongbregas@icloud.com>
2021-09-01 23:15:19 -0700

    fixing Heroku ico

--- pokerfaze/src/pages/Home/Home.js
+++ pokerfaze/src/pages/Home/Home.js
@@ -4,7 +4,7 @@ import React from 'react';
 const Home = () => {
     return (
         <div className="home">
-            <h1>Home</h1>
+            <h1>Home is under construction. Please check My Hardware.</h1>
         </div>
     );
 }
